13d520d89defdc1cddd1dd971d30c421f1937f5e
Eric Frommelt <ericfrommelt@gmail.com>
2018-05-20 15:23:46 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,12 +1,11 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  !(board[index] == " " || "" || nil) || (board[index] == "X" || "O")
 
-  # if board[index] == " " || "" || nil
-  #   return false
-  # elsif board[index] == "X" || "O"
-  #   return true
-  # end
+
+  if board[index] == " " || "" || nil
+    return false
+  elsif board[index] == "X" || "O"
+  end
 
 end
