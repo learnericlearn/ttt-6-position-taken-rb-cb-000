84d019b6ed87aee072fbb3c02c61c9e9d141fea4
Eric Frommelt <ericfrommelt@gmail.com>
2018-05-20 14:32:39 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,9 +4,8 @@ def position_taken?(board, index)
 
   if board[index] == " " || "" || nil
     return false
-  end
-
-  if board[index] == "X" || "O"
+  elsif board[index] == "X" || "O"
     return true
   end
+
 end
